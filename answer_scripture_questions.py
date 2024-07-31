077f9997d817fccd2daaf9e4d2e16ce8999861e0
Zachary Clement <clementzach@icloud.com>
2024-07-31 13:14:22 -0400

    Use openai for generation

--- answer_scripture_questions.py
+++ answer_scripture_questions.py
@@ -1,8 +1,15 @@
+import os
 import ollama
 import chromadb
 from create_vector_store import EMBEDDINGS_MODEL, CHROMA_PATH, COLLECTION_NAME
+from openai import OpenAI
+from config import OPENAI_API_KEY
 
-GENERATIVE_MODEL = "llama3"
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+
+GENERATIVE_MODEL = "gpt-4o-mini"
+
+
 def get_clients():
     client = ollama.Client()
     chroma_client = chromadb.PersistentClient(path=CHROMA_PATH)
@@ -10,25 +17,29 @@ def get_clients():
     return (client, collection)
 
 def interpret_verses(question, verses):
-    verses_string = "Some scriptures that might help you with the answer to your question are:\n\n" + "\n".join(verses)
-    prompt = f"""
-    You are a concise, yet thoughtful latter-day saint seminary teacher. 
+    messages = []
     
-    Your job is to answer the following question:
-    ```
-    {question}
-    ```
-
-    Complete the following statement to share your response to the question. Use some of the scriptures to support your thinking, but you don't have to quote from all of them. You should use three to five sentences to synthesize what is shared in the verses.
-    
-    <<The response starts here>>
+    verses_string = "\n".join(verses)
+    sys_prompt = f"""
+    You are a concise, faithful, and thoughtful latter-day saint. Your job is to answer your friend's question by quoting from some of the following scriptures: 
     {verses_string}
+
+    Use some of the scriptures to support your thinking, but you don't have to quote from all of them. You should use three to five sentences to synthesize what is shared in the verses. and help your friend understand the answer to their question.
     
     """
+    
+    print("Here are some scriptures that may be helpful:")
     print(verses_string)
-    results = ollama.generate(model=GENERATIVE_MODEL, prompt = prompt)
-    print(results["response"])
-    return (verses_string, results)
+
+    client = OpenAI()
+    response = client.chat.completions.create(
+  model=GENERATIVE_MODEL,
+  messages=[
+    {"role": "system", "content": sys_prompt},
+    {"role": "user", "content": question}
+    ]
+    )
+    print(response.choices[0].message.content)
 
 def answer_question(client, collection, num_verses = 5):
     question = input("Enter a question:\n")
