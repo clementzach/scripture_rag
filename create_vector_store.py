adbfbdbe34aa27f568a0b645c1e9775b3af3fd5c
Zachary Clement <clementzach@icloud.com>
2024-07-31 14:58:11 -0400

    Add to the collection after each embedding instead of all at once

--- create_vector_store.py
+++ create_vector_store.py
@@ -43,7 +43,7 @@ def get_chroma_collection(chroma_path):
     collection = chroma_client.get_or_create_collection(name=COLLECTION_NAME)
     return collection
 
-def get_embeddings(documents, ids, collection):
+def add_to_collection(documents, ids, collection):
     """Create embeddings for any documents that aren't already in the collection"""
     # Initialize the Ollama client
     client = ollama.Client()
@@ -56,24 +56,16 @@ def get_embeddings(documents, ids, collection):
         if len(collection.get(ids[i])['ids']) > 0:
             continue ## No need to get embeddings because we already have them stored. 
         response = client.embeddings(model=EMBEDDINGS_MODEL, prompt=doc)
-        embeddings.append(response["embedding"])
-        ids_to_add.append(ids[i])
-        documents_to_add.append(documents[i])
-    return (embeddings, documents_to_add, ids_to_add)
-
-def add_to_vector_store(ids, embeddings, documents, collection, iteration_size = 10_000):
-    for i in range(int(len(ids) / iteration_size)):
-        ## There is a max batch size for collection, so we can't add them all at once. 
-        collection.add(ids = ids[i * iteration_size:(i + 1) * iteration_size],
-                        embeddings = embeddings[i * iteration_size:(i + 1) * iteration_size],
-                       documents = documents[i * iteration_size:(i + 1) * iteration_size])
+        collection.add(ids = [ids[i]],
+                        embeddings = [response["embedding"]],
+                       documents = [doc])
+        
 
 
 def main():
     documents, ids = parse_scriptures(DATA_PATH)
     collection = get_chroma_collection(CHROMA_PATH)
-    embeddings, ids_to_add, documents_to_add = get_embeddings(documents, ids, collection)
-    add_to_vector_store(ids_to_add, embeddings, documents_to_add, collection)
+    add_to_collection(documents, ids, collection)
 
 if __name__ == "__main__":
     main()
