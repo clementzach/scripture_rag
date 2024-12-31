daf7c626fa8d0f3cbe23281ff4dfe785ed07243d
Zachary Clement <clementzach@icloud.com>
2024-12-30 19:52:50 -0500

    Simplify functions, make prompt clearer, ensure questions are generated for long chapters

--- make_hypothetical_questions.py
+++ make_hypothetical_questions.py
@@ -7,26 +7,21 @@ GENERATIVE_MODEL = "gpt-4o-mini"
 
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
-def get_all_chapters(fp):
-    """Get all availabale chapters in scripture"""
-    chapters_list = set()
-    file = open(fp, 'r')
-    for line in file:
-        current_chapter = line.split(":")[0]     
-        chapters_list.add(current_chapter)
-    return chapters_list
 
-def get_chapter_text(fp, chapter_to_find):
+def get_chapters_dict(fp):
     """
     Get text corresopnding to a chapter of scripture
     """
-    output_text = ""
+    chapters_dict = dict()
     file = open(fp, 'r')
     for line in file:
         current_chapter = line.split(":")[0]
-        if current_chapter == chapter_to_find:
-            output_text +=  line
-    return output_text
+        if current_chapter in chapters_dict:
+            chapters_dict[current_chapter] += line
+        else:
+            chapters_dict[current_chapter] = line
+
+    return chapters_dict
 
 def get_hypothetical_questions(chapter_text):
     system_prompt = f"""You are a knowledgeable, faithful, compassionate, and thoughtful latter-day saint. Your role is to anticipate the questions that can be answered from a given section of scripture to enable actual user questions to be efficiently mapped to verse references. 
@@ -34,7 +29,7 @@ def get_hypothetical_questions(chapter_text):
 The questions generated should be something that a Latter-day Saint in the current day may ask as they are trying to understand a verse in a different part of scripture or if they are trying to remember a fact about the story in the scripture. 
 
 
-For each verse provided, generate: 
+For each verse provided, generate exactly three questions: 
 1. A question that asks about the general facts of the verse
 2. A question about the life application of the verse
 3. A question about the doctrine taught in the verse
@@ -66,10 +61,21 @@ Alma 32:11 Do we need to be in a synagogue to worship god?
 def main():
     questions_fp = "data/hypothetical_questions.txt"
     scriptures_fp = "data/scriptures.txt"
-    all_chapters = get_all_chapters(scriptures_fp)
-    for chapter in all_chapters:
-        chapter_text = get_chapter_text(scriptures_fp, chapter)
+    all_chapters = get_chapters_dict(scriptures_fp)
+    for chapter in all_chapters.keys():
+        chapter_text = all_chapters[chapter]
         questions = get_hypothetical_questions(chapter_text)
+        ## For long chapters, gpt has trouble writing three questions per verse. In this case, we'll split the chapters into sections of 10 verses. 
+        if len(questions.split("\n")) < (len(chapter_text.split("\n")) * 2):
+            questions = ""
+            current_chapter_text = ""
+            verses = chapter_text.split("\n")
+            min_verse = 0
+            increment_size = 10
+            overlap_size = 2
+            while (min_verse) < (len(verses)):
+                questions = questions + "\n" + get_hypothetical_questions("\n".join(verses[min_verse:min(len(verses), (min_verse + increment_size))]))
+                min_verse += increment_size - overlap_size ## We will have a 2-verse overlap, which is probably not ideal. Perhaps later I should do something to ensure that there is the same number per verse. 
         with open(questions_fp, "a") as f:
             f.write(questions + "\n")
 
