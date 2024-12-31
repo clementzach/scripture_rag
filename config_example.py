7258401368cea9c8213314fe99a79c70838eea84
Zachary Clement <clementzach@icloud.com>
2024-12-31 00:34:26 -0500

    Add hypothetical question data

--- config_example.py
+++ config_example.py
@@ -5,3 +5,7 @@ DATA_PATH = os.path.join('data','scriptures.txt')
 EMBEDDINGS_MODEL = 'nomic-embed-text'
 CHROMA_PATH = "chroma_dir"
 COLLECTION_NAME = "verses"
+HYP_QUEST_COLLECTION_NAME = "hypothetical_question"
+HYP_QUEST_EMBEDS_MODEL = "text-embedding-3-small"
+HYP_QUEST_PATH = os.path.join('data','hypothetical_questions.txt')
+HYP_QUEST_ID_DELIM = "|"
