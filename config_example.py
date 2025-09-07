5f8195fb0a34d441e3cb63a33b9058663e9da9ed
Zachary Clement <clementzach@icloud.com>
2025-09-06 22:16:38 -0400

    Add hypothetical question paths back in

--- config_example.py
+++ config_example.py
@@ -11,3 +11,9 @@ COLLECTION_NAME = "verses"
 CHROMA_USE_HTTP = True
 CHROMA_SERVER_HOST = os.environ.get("CHROMA_SERVER_HOST", "localhost")
 CHROMA_SERVER_HTTP_PORT = int(os.environ.get("CHROMA_SERVER_HTTP_PORT", "8000"))
+
+
+HYP_QUEST_COLLECTION_NAME = "hypothetical_question"
+HYP_QUEST_EMBEDS_MODEL = "text-embedding-3-small"
+HYP_QUEST_PATH = os.path.join('data','hypothetical_questions.txt')
+HYP_QUEST_ID_DELIM = "|"
